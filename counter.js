64b15db75f4050c7846dd47e8efb60aa1ef58daf
David Lazic <lazic.ddavid@gmail.com>
2025-06-22 11:55:36 +0200

    added if else

--- counter.js
+++ counter.js
@@ -1,3 +1,16 @@
 let count = 0;
 const buttons = document.querySelectorAll(".btn");
 const value = document.getElementById("value");
+
+buttons.forEach(function (btn) {
+  btn.addEventListener("click",   function (e){
+    const action = e.target.classList;
+
+    if (action.contains("minus")) {
+      count--;
+    } else if (action.contains("plus")) {
+      count++;
+    } else {
+      count = 0;
+  
+}
