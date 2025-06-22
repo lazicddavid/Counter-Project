c23a0324867a1d1efa9e1c256c8ae96d15afea4e
David Lazic <lazic.ddavid@gmail.com>
2025-06-22 12:33:48 +0200

    added btn

--- counter.js
+++ counter.js
@@ -3,14 +3,25 @@ const buttons = document.querySelectorAll(".btn");
 const value = document.getElementById("value");
 
 buttons.forEach(function (btn) {
-  btn.addEventListener("click",   function (e){
+  btn.addEventListener("click", function (e) {
     const action = e.target.classList;
 
     if (action.contains("minus")) {
       count--;
     } else if (action.contains("plus")) {
       count++;
-    } else {
+    } else if (action.contains("reset")) {
       count = 0;
-  
-}
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count < 0) {
+      value.style.color = "red";
+    } else {
+      value.style.color = "#333";
+    }
+
+    value.textContent = count;
+  });
+});
